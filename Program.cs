6c49b355f666ec20026f045281e962bd548c82eb
Todd Bennett <tebennett@eprod.com>
2021-06-24 09:35:21 -0500

    Initial push

--- Program.cs
+++ Program.cs
@@ -1,15 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Core.Applications.CommandProcessing;
+using Core.Computers;
+using Core.Monads;
+using static Core.Monads.MonadFunctions;
 
 namespace FFind
 {
-   class Program
+   public class Program : CommandProcessor
    {
-      static void Main(string[] args)
+      public static void Main(string[] args)
+      {
+         var program = new Program("FFind");
+      }
+
+      public Program(string application) : base(application)
       {
       }
+
+      public override void Initialize()
+      {
+         Folder = none<FolderName>();
+         File = none<FileName>();
+      }
+
+      [Command("find"), CommandHelp("Finds text by pattern","folder?", "file?")]
+      public void Find()
+      {
+         if (Folder.If(out var folder))
+         {
+
+         }
+         else if (File.If(out var file))
+         {
+
+         }
+         else
+         {
+
+         }
+      }
+
+      [Switch("pattern"), ShortCut("p"), SwitchHelp("pattern")]
+      public string Pattern { get; set; }
+
+      [Switch("folder"), ShortCut("F"), SwitchHelp("folder")]
+      public Maybe<FolderName> Folder { get; set; }
+
+      [Switch("file"), ShortCut("f"), SwitchHelp("file")]
+      public Maybe<FileName> File { get; set; }
    }
 }
